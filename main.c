bb4b44db041d6412cc8594d2cebf32b9b0bec9d2
Cliff Snyder <cliff.snyder@gmail.com>
2023-02-10 19:43:35 -0500

    decls

--- main.c
+++ main.c
@@ -5,12 +5,14 @@ int yyparse (json_t **j);
 int
 main (int argc, char *argv[])
 {
-  json_t *j;
-  int r = yyparse (&j);
-  if (r == 0)
+  json_t *j = 0;
+  int rc = 0;
+
+  rc = yyparse (&j);
+  if (rc == 0)
     {
       json_t_pprint (stdout, j);
       free_json_t(j);
     }
-  return (r);
+  return (rc);
 }
